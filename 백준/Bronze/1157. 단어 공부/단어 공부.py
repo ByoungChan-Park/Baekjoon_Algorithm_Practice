d724b39007976a472babe101c61c805f41300f4f
ByoungChan-Park <90272469+ByoungChan-Park@users.noreply.github.com>
2022-05-09 20:05:28 +0900

    [Bronze I] Title: 단어 공부, Time: 240 ms, Memory: 32796 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,19 @@
+string = input().upper()
+cnt = {}
+
+for i in string:
+      if i in cnt :
+        cnt[i] += 1
+      else :
+        cnt[i] = 1
+
+val = []
+cnt_max = max(cnt.values())
+for j, k in cnt.items():
+      if k == cnt_max:
+        val.append(j)
+
+if len(val) > 1:
+      print("?")
+else :
+      print("".join(val))
