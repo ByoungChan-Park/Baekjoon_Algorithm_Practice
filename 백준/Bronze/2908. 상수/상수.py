858f025f886a5399d80c3d061661608c794132f5
ByoungChan-Park <90272469+ByoungChan-Park@users.noreply.github.com>
2022-05-09 23:42:59 +0900

    [Bronze II] Title: 상수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,11 +1,6 @@
 num1, num2 = input().split()
 
-num1_list = []
-for i in num1 : 
-    num1_list.append(i)
+num1 = int(num1[::-1])
+num2 = int(num2[::-1])
 
-num2_list = []
-for i in num2 : 
-    num2_list.append(i)
-
-print(max(int(''.join(reversed(num1_list))), int(''.join(reversed(num2_list)))))
+print(max(num1, num2))
